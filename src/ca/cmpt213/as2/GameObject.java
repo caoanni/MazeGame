376a56044a7ba930f1484816cd90a14b56d53d78
annic <annic@sfu.ca>
2017-02-15 23:50:15 -0800

    Implement abstract GameObject class

--- src/ca/cmpt213/as2/GameObject.java
+++ src/ca/cmpt213/as2/GameObject.java
@@ -1,19 +1,29 @@
 package ca.cmpt213.as2;
 
 /**
+ *
  * Created by anni on 2/12/17.
  */
-public class GameObject {
+public abstract class GameObject {
 
-    private int[] currentPos;
-    private char symbol;
+    protected int[] currentPos; //[0] stores row position, [1] stores column position
+    protected char symbol;
+
+    public GameObject(int[] startPosition, char symbol) {
+        this.currentPos = new int[2];
+        this.currentPos[0] = startPosition[0];
+        this.currentPos[1] = startPosition[1];
+        this.symbol = symbol;
+    }
 
     public int[] getPosition(){
         return currentPos;
     }
 
-    //public boolean move(Maze maze){
-    //}
+    public char getSymbol() {
+        return symbol;
+    }
 
+    public abstract boolean move(int[] direction, Maze maze);
 
 }
