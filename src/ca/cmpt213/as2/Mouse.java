ab0c746dc9da57979723f8d688c309d200cf2b98
annic <annic@sfu.ca>
2017-02-15 23:50:53 -0800

    Implement Mouse class

--- src/ca/cmpt213/as2/Mouse.java
+++ src/ca/cmpt213/as2/Mouse.java
@@ -3,27 +3,50 @@ package ca.cmpt213.as2;
 /**
  * Created by anni on 2/12/17.
  */
-public class Mouse {
+public class Mouse extends GameObject {
 
     private int cheeseFound;
 
-    public int getCheeseFound(){
+    public Mouse(int[] direction, char symbol) {
+        super(direction,symbol);
+        this.cheeseFound = 0;
+    }
+
+    public int getCheeseFound() {
         return cheeseFound;
     }
 
-    //needs to override
-    //public boolean move(Maze maze){
-    //}
-
-    private void incrementCheese(){
-        cheeseFound++;
+    public void isCheese(Cheese cheese, Maze maze) {
+        if (this.currentPos[0] == cheese.currentPos[0] &&
+                this.currentPos[1] == cheese.currentPos[1]) {
+            cheeseFound++;
+            cheese.move(cheese.getPosition(), maze);
+        }
     }
 
-    //private boolean isValidMove(Maze maze){
-    //}
+    @Override
+    public boolean move(int[] direction, Maze maze) {
+        if (isValidMove(direction, maze)) {
+            currentPos[0] += direction[0];
+            currentPos[1] += direction[1];
+            updateMaze(maze, currentPos);
+            return true;
+        }
+        return false;
+    }
 
-    //private void updateMaze(Maze maze){
-    //    maze.updateMaze();
-    //}
 
+    private boolean isValidMove(int[] direction, Maze maze) {
+        int row = currentPos[0] + direction[0];
+        int col = currentPos[1] + direction[1];
+        char[][] fullMaze = maze.getFullMaze();
+        if (fullMaze[row][col] == '#') {
+            return false;
+        }
+        return true;
+    }
+
+    private void updateMaze(Maze maze, int[] newPosition) {
+        //maze.updateMaze(newPosition);
+    }
 }
